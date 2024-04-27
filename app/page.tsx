1849193887fa361ee7d76dbf5875a33e07f131dd
Tejas <tejaskwagh200@gmail.com>
2024-04-27 18:00:33 +0530

    Title changed

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Form from "@/components/Form";
 export default function Home() {
   return (
     <>
-      <h1>Latest Todos App</h1>
+      <h1> Todos App</h1>
      <div className="flex items-center justify-center w-full h-screen">
       <Form/>
      </div>
