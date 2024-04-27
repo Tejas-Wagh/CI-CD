b95dac9918a973911f37ec23778dd71462bdb584
Tejas <tejaskwagh200@gmail.com>
2024-04-27 13:08:36 +0530

    Initial commit

---
+++ components/Form.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useState } from "react";
+
+function Form() {
+  const [todos, setTodos] = useState<string[]>([]);
+  const [todo, setTodo] = useState<string>("");
+
+  function submitTodo() {
+    setTodos((prev: string[]) => {
+      return [...prev, todo];
+    });
+  }
+
+  return (
+    <div>
+      <div className="space-x-4">
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={(e) => setTodo(e.target.value)}
+          className="border-2 border-black"
+        />
+        <button onClick={submitTodo}>Submit</button>
+      </div>
+        <div className="mt-8">
+            <h2>Todos</h2>
+            <ul>
+                {todos.map((todo)=><li key={todo}>{todo}</li>)}
+            </ul>
+        </div>
+    </div>
+  );
+}
+
+export default Form;
